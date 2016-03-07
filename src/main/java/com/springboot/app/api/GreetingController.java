50e36a107af56a5a1d937a8d87985cd9fc03f1ed
Loknath Behera <loknathbehera@live.com>
2016-03-07 23:47:48 +0530

    Update and DELETE controller maping Function created
    Delete is not function properly plz have a look

--- src/main/java/com/springboot/app/api/GreetingController.java
+++ src/main/java/com/springboot/app/api/GreetingController.java
@@ -1,6 +1,7 @@
 package com.springboot.app.api;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -27,6 +28,19 @@ public class GreetingController {
 			greetingMap = new HashMap<BigDecimal, Greeting>();
 			nextId = BigDecimal.ONE;
 		}
+		// updates.......
+
+		if (greeting.getId() != null) {
+			Greeting oldGreeting = greetingMap.get(greeting.getId());
+			if (oldGreeting == null) {
+				return null;
+			}
+			greetingMap.remove(greeting.getId());
+			greetingMap.put(greeting.getId(), greeting);
+
+			return greeting;
+		}
+
 		greeting.setId(nextId);
 		nextId = nextId.add(BigDecimal.ONE);
 		greetingMap.put(greeting.getId(), greeting);
@@ -42,6 +56,16 @@ public class GreetingController {
 		greeting2.setText("Hi Chiku");
 		save(greeting2);
 
+	}
+
+	public static boolean delete(BigInteger id) {
+
+		Greeting deletedGreeting = greetingMap.remove(id);
+		if (deletedGreeting != null) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@RequestMapping(value = "api/greetings", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -63,8 +87,29 @@ public class GreetingController {
 	@RequestMapping(value = "api/greetings", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Greeting> createGreeting(@RequestBody Greeting greeting) {
 
-		Greeting savedGreeting=save(greeting);
-		return new ResponseEntity<Greeting>(savedGreeting,HttpStatus.CREATED);
+		Greeting savedGreeting = save(greeting);
+		return new ResponseEntity<Greeting>(savedGreeting, HttpStatus.CREATED);
+	}
+
+	@RequestMapping(value = "api/greetings/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> updateGreeting(@RequestBody Greeting greeting) {
+		Greeting updatedGreeting = save(greeting);
+		if (updatedGreeting == null) {
+			return new ResponseEntity<Greeting>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<Greeting>(updatedGreeting, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "api/greetings/{id}", method = RequestMethod.DELETE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Greeting> deleteGreeting(@PathVariable(value = "id") BigInteger id,
+			@RequestBody Greeting greeting) {
+		boolean deleted = delete(id);
+		if (!deleted) {
+			return new ResponseEntity<Greeting>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<Greeting>(HttpStatus.NO_CONTENT);
 	}
 
 }
