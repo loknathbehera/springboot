bd196e08baf2b8cd22808f4b54d6e04f87892152
Loknath Behera <loknathbehera@live.com>
2016-03-09 22:57:16 +0530

    Transaction Management implemented

--- src/main/java/com/springboot/app/services/GreetingServiceBean.java
+++ src/main/java/com/springboot/app/services/GreetingServiceBean.java
@@ -4,11 +4,14 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springboot.app.model.Greeting;
 import com.springboot.app.repository.GreetingRepository;
 
 @Service
+@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 public class GreetingServiceBean implements GreetingService {
 
 	@Autowired
@@ -25,6 +28,7 @@ public class GreetingServiceBean implements GreetingService {
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting create(Greeting greeting) {
 
 		if (greeting.getId() != null) {
@@ -35,6 +39,7 @@ public class GreetingServiceBean implements GreetingService {
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Greeting update(Greeting greeting) {
 		Greeting persistetGreeting = findOne(greeting.getId());
 		if (persistetGreeting == null) {
@@ -42,10 +47,16 @@ public class GreetingServiceBean implements GreetingService {
 		}
 
 		Greeting updatedGreeting = greetingRepository.save(greeting);
+		
+		if(greeting.getId()==3){
+			throw new RuntimeException("Bang Bang");
+		}
+		
 		return updatedGreeting;
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public void delete(Long id) {
 
 		Greeting greeting = greetingRepository.findOne(id);
